b7ae9ef1a3025257248d49491407f18262028ef5
chinasa056 <chinasaacha05@gmail.com>
2025-01-24 17:18:29 +0100

    made a change to the model

--- models/products.js
+++ models/products.js
@@ -1,51 +1,83 @@
-const { Sequelize, DataTypes, Model } = require('sequelize');
-const sequelize = new Sequelize('ecommerce', 'root', 'Acha105#', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+// const { Sequelize, DataTypes, Model } = require('sequelize');
+// const sequelize = new Sequelize('ecommerce', 'root', 'Acha105#', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+// });
 
-class Product extends Model {}
+// class Product extends Model {}
 
-// Define the Product model
-Product.init(
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: Sequelize.UUIDV4, // Automatically generate a UUID
-      allowNull: false,
-      primaryKey: true,
-    },
-    storeId: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      references: {
-        model: 'stores', // Refers to the Store model
-        key: 'id', // The key in the Store model that this foreign key references
-      },
-    },
-    productName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    productQTY: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    productAmount: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize, // Pass the sequelize instance
-    modelName: 'Product', // Define the model name
-    tableName: 'products', // Name of the table in the database
-    timestamps: true, // Automatically adds createdAt and updatedAt columns
+// // Define the Product model
+// Product.init(
+//   {
+//     id: {
+//       type: DataTypes.UUID,
+//       defaultValue: Sequelize.UUIDV4, // Automatically generate a UUID
+//       allowNull: false,
+//       primaryKey: true,
+//     },
+//     storeId: {
+//       type: DataTypes.UUID,
+//       allowNull: false,
+//       references: {
+//         model: 'stores', // Refers to the Store model
+//         key: 'id', // The key in the Store model that this foreign key references
+//       },
+//     },
+//     productName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     productQTY: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//     },
+//     productAmount: {
+//       type: DataTypes.DECIMAL,
+//       allowNull: false,
+//     },
+//   },
+//   {
+//     sequelize, // Pass the sequelize instance
+//     modelName: 'Product', // Define the model name
+//     tableName: 'products', // Name of the table in the database
+//     timestamps: true, // Automatically adds createdAt and updatedAt columns
+//   }
+// );
+
+// // Define associations (if any)
+// Product.associate = (models) => {
+//   Product.belongsTo(models.Store, { foreignKey: 'storeId', as: 'store' });
+// };
+// module.exports = Product;
+
+
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class products extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The models/index file will call this method automatically.
+     */
+    static associate(models) {
+      
+  products.associate = (models) =>{
+    products.belongsTo(models.stores,{foreignKey:"storeId",as:"stores"})
   }
-);
-
-// Define associations (if any)
-Product.associate = (models) => {
-  Product.belongsTo(models.Store, { foreignKey: 'storeId', as: 'store' });
+      // define association here
+    }
+  }
+  products.init({
+    storeId: DataTypes.UUID,
+    productName: DataTypes.STRING,
+    productQty: DataTypes.INTEGER,
+    productAmount: DataTypes.DECIMAL
+  }, {
+    sequelize,
+    modelName: 'products',
+  });
+  return products;
 };
-module.exports = Product;
--- models/stores.js
+++ models/stores.js
@@ -1,46 +1,77 @@
-  const { Sequelize, DataTypes, Model } = require('sequelize');
-  const sequelize = new Sequelize('ecommerce', 'root', 'Acha105#', {
-    host: 'localhost',
-    dialect: 'mysql' 
-  });
+  // const { Sequelize, DataTypes, Model } = require('sequelize');
+  // const sequelize = new Sequelize('ecommerce', 'root', 'Acha105#', {
+  //   host: 'localhost',
+  //   dialect: 'mysql' 
+  // });
 
 
-  class Store extends Model {}
+  // class Store extends Model {}
 
-  // Define the Store model
-  Store.init(
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: Sequelize.UUIDV4, // Automatically generate a UUID
-        allowNull: false,
-        primaryKey: true,
-      },
-      storeName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      location: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    },
-    {
-      sequelize, // Pass the sequelize instance
-      modelName: 'Store', // Define the model name
-      tableName: 'stores', // Name of the table in the database
-      timestamps: true, // Automatically adds createdAt and updatedAt columns
+  // // Define the Store model
+  // Store.init(
+  //   {
+  //     id: {
+  //       type: DataTypes.UUID,
+  //       defaultValue: Sequelize.UUIDV4, // Automatically generate a UUID
+  //       allowNull: false,
+  //       primaryKey: true,
+  //     },
+  //     storeName: {
+  //       type: DataTypes.STRING,
+  //       allowNull: false,
+  //     },
+  //     email: {
+  //       type: DataTypes.STRING,
+  //       allowNull: false,
+  //       unique: true,
+  //     },
+  //     location: {
+  //       type: DataTypes.STRING,
+  //       allowNull: false,
+  //     },
+  //   },
+  //   {
+  //     sequelize, // Pass the sequelize instance
+  //     modelName: 'Store', // Define the model name
+  //     tableName: 'stores', // Name of the table in the database
+  //     timestamps: true, // Automatically adds createdAt and updatedAt columns
+  //   }
+  // );
+
+  // // Define associations (if any)
+  // Store.associate = (models) => {
+  //   Store.hasOne(models.Product, { foreignKey: 'storeId', as: 'products' });
+  // };
+
+  // module.exports = Store;
+
+
+  'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class stores extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The models/index file will call this method automatically.
+     */
+    static associate(models) {
+      stores.associate=(models)=>{
+        stores.hasOne(models.products,{foreignKey:"storeid",as:"products"})
+    
+      }
+      // define association here
     }
-  );
-
-  // Define associations (if any)
-  Store.associate = (models) => {
-    Store.hasOne(models.Product, { foreignKey: 'storeId', as: 'products' });
-  };
-
-  module.exports = Store;
+  }
+  stores.init({
+    storeName: DataTypes.STRING,
+    location: DataTypes.STRING,
+    email: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'stores',
+  });
+  return stores;
+}
